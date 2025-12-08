e915dc646e48729bde8c84dc811f7c5db7ff2b10
Theo Laran <teonommay@yahoo.fr>
2025-12-08 19:46:59 +0100

    feat(2025): add day 07 solution 2

--- 2025/07/solution.py
+++ 2025/07/solution.py
@@ -10,15 +10,22 @@ with open(INPUT_PATH, "r") as inpf:
 
 # --- Main solution ---#
 ttl_split = 0
-SOLUTION = 1
+SOLUTION = 2
 
 # Save the position of falling beams
 beams_x_positions = set()
 
+# Link every x curr position to his timeline
+beams_x_timelines = {}
+
 # Find the first beams in the list
 beams_x_positions.add(STREAM[0].index("S"))
 
-for line in STREAM:
+# Init timelines, at the beggining we have one only
+beams_x_timelines[STREAM[0].index("S")] = 1
+
+for i, line in enumerate(STREAM):
+    print(f"{i}/{len(STREAM)}")
     # Find all splitter on the line
     splitter_idx = [i for i, s in enumerate(line) if s == SPLITTER]
 
@@ -36,4 +43,23 @@ for line in STREAM:
             # Increase nb of split
             ttl_split += 1
 
-print(ttl_split) 
+            # Create empty shell for next values
+            if max(idx - 1, 0) not in beams_x_timelines:
+                beams_x_timelines[max(idx - 1, 0)] = 0
+            if min(idx + 1, len(line)) not in beams_x_timelines:
+                beams_x_timelines[min(idx + 1, len(line))] = 0
+
+            # Update these value with the current number of timelines
+            beams_x_timelines[max(idx - 1, 0)] += beams_x_timelines[idx]
+            ###
+            beams_x_timelines[min(idx + 1, len(line))] += beams_x_timelines[idx]
+
+            # We are on a splitter, delete current position after treatment
+            beams_x_timelines[idx] = 0
+
+ttl_timeline = 0
+for x_tl in beams_x_timelines.values():
+    ttl_timeline += x_tl
+
+
+print(ttl_timeline)
